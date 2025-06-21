b7744c4e0906abf864729a602139070aa6eed02f
alibartwo <ali@alibartu.com>
2025-06-21 03:46:25 +0300

    chore: initialize the project

---
+++ components/layout/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="site-header">
+    <div class="container">
+      <div class="logo">
+        <NuxtLink to="/">
+          <img src="/onofset-logo.png" alt="Logo" />
+        </NuxtLink>
+      </div>
+      <nav class="main-nav">
+        <NuxtLink to="/">Anasayfa</NuxtLink>
+        <NuxtLink to="/hakkimizda">Hakkımızda</NuxtLink>
+        <NuxtLink to="/blog">Blog</NuxtLink>
+        <NuxtLink to="/faq">Müşteri Yorumları</NuxtLink>
+        <NuxtLink to="/iletisim">İletişim</NuxtLink>
+      </nav>
+      <div class="actions">
+        <NuxtLink to="/sepet" class="cart">
+          <Icon name="heroicons-outline/shopping-cart" />
+        </NuxtLink>
+      </div>
+    </div>
+  </header>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+    <Header />
+    <main><slot></slot></main>
+    <Footer />
+</template>
+<script setup lang="ts">
+import Header from '@/components/layout/Header.vue'
+import Footer from '@/components/layout/Footer.vue'
+</script>
