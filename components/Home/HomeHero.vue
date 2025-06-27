fb9b503f2448bef9c82501f0750078a555026e3a
alibartwo <ali@alibartu.com>
2025-06-27 13:44:10 +0300

    dev: development

--- components/Home/HomeHero.vue
+++ components/Home/HomeHero.vue
@@ -1,38 +1,19 @@
 <template>
   <section class="py-4 md:py-10 min-h-[500px]">
-    <div
-      class="grid items-center max-w-screen-xl grid-cols-12 gap-4 px-4 mx-auto md:gap-4"
-    >
+    <div class="grid items-center max-w-screen-xl grid-cols-12 gap-4 px-4 mx-auto md:gap-4">
       <!-- Left content -->
-      <div
-        class="flex flex-col justify-between h-full col-span-12 gap-1 p-4 md:col-span-4 bg-secondary rounded-3xl md:p-8"
-      >
+      <div class="flex flex-col justify-between h-full col-span-12 gap-1 p-4 md:col-span-4 bg-secondary rounded-3xl md:p-8">
         <!-- Badge -->
         <div class="flex items-center gap-3">
           <div class="flex -space-x-2">
-            <NuxtImg
-              src="/images/testimonials/avatar1.jpg"
-              alt="avatar1"
-              width="32"
-              height="32"
-              class="object-cover border border-gray-600 rounded-full"
-            />
+            <NuxtImg src="/images/testimonials/avatar1.jpg" alt="avatar1" width="32" height="32"
+              class="object-cover border border-gray-600 rounded-full" />
 
-            <NuxtImg
-              src="/images/testimonials/avatar2.jpg"
-              alt="avatar2"
-              width="32"
-              height="32"
-              class="object-cover border border-gray-600 rounded-full"
-            />
+            <NuxtImg src="/images/testimonials/avatar2.jpg" alt="avatar2" width="32" height="32"
+              class="object-cover border border-gray-600 rounded-full" />
 
-            <NuxtImg
-              src="/images/testimonials/avatar3.jpg"
-              alt="avatar3"
-              width="32"
-              height="32"
-              class="object-cover border border-gray-600 rounded-full"
-            />
+            <NuxtImg src="/images/testimonials/avatar3.jpg" alt="avatar3" width="32" height="32"
+              class="object-cover border border-gray-600 rounded-full" />
           </div>
           <p class="text-sm font-light text-primary">
             50.000+ etkinlikte tercih edildi.
@@ -47,13 +28,7 @@
             korumanız hazır
           </h1>
 
-          <BaseButton
-            to="/hakkimizda"
-            type="primary"
-            label="Ürünü İncele"
-            icon="uil:arrow-right"
-            iconSize="20"
-          />
+          <BaseButton to="/hakkimizda" type="primary" label="Ürünü İncele" icon="uil:arrow-right" iconSize="20" />
 
           <p class="text-xs text-gray-500">
             100×120cm Tek Kullanımlık Masa ve Aktivite Örtüsü
@@ -63,11 +38,7 @@
 
       <!-- Right image -->
       <div class="h-full col-span-12 overflow-hidden md:col-span-8">
-        <img
-          src="/images/hero-img.png"
-          alt="Hero Görsel"
-          class="object-cover w-full h-full rounded-3xl"
-        />
+        <img src="/images/hero-img.png" alt="Hero Görsel" class="object-cover w-full h-full rounded-3xl" />
       </div>
     </div>
   </section>
